92ac3a4070ca3fcd5f873fce0f3826131de36de2
jose rennyer <joserennyer30@gmail.com>
2023-07-14 18:09:23 -0300

    criado as funções de desabilitar classificações e verificações do que foi digitado

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,13 +3,14 @@ const weightField = document.querySelector("#weight");
 const calcImcButton = document.querySelector(".button-calc");
 const imcArea = document.querySelector(".imc-area-response");
 const imcAreaResponse = document.querySelector(".imc-response");
+const imcClassifications = document.querySelectorAll(".classification");
 
-function activeImcArea() {
-    imcArea.classList.remove("invisible");
-}
 
+// Calc IMC function and write Imc Value
 function writeImc(value) {
-    imcAreaResponse.innerHTML = value.toFixed(2);
+    imcAreaResponse.innerHTML = Number.isNaN(value) ? "0.0":value;
+
+    activeImcArea();
 }
 
 function calcImcValue(height, weight) {
@@ -19,16 +20,59 @@ function calcImcValue(height, weight) {
 function calcImc() {
     const height = Number(heightField.value.replace(",", "."));
     const weight = Number(weightField.value.replace(",", "."));
-
-    const imc = calcImcValue(height, weight);
+    const imc = Number(calcImcValue(height, weight).toFixed(2));
 
     writeImc(imc);
-    activeImcArea();
+    verifyImcClassification(imc);
 }
 
 
 
+// functions of rendering and verifications
+function activeImcArea() {
+    imcArea.classList.remove("invisible");
+}
+
+function verifyImcClassification(imc) {
+    switch(true) {
+        case imc >= 0 && imc < 18.5:
+            disableFields("class-1");
+            break;
+        case imc >= 18.5 && imc <= 24.99:
+            disableFields("class-2");
+            break;
+        case imc >= 25.0 && imc <= 29.99:
+            disableFields("class-3");
+            break;
+        case imc >= 30.0 && imc <= 34.99:
+            disableFields("class-4");
+            break;
+        case imc >= 35.0 && imc <= 39.99:
+            disableFields("class-5");
+            break;
+        case imc >= 40.0:
+            disableFields("class-6");
+            break;
+        default:
+            disableFields(imc);
+    }
+}
+
+function resetFields() {
+    imcClassifications.forEach(imc => imc.classList.remove("disabled"))
+}
+
+function disableFields(freeField) {
+    resetFields();
+
+    if(freeField !== undefined && !Number.isNaN(freeField)) {
+        imcClassifications.forEach(imc => {
+            if(!imc.classList.contains(freeField)) {
+                imc.classList.add("disabled");
+            }
+        });
+    }
+}
 
 
 calcImcButton.addEventListener("click", calcImc);
-
